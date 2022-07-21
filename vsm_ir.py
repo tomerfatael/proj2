86d5a206dd0f7e07b3d8a52b6dc82fcab3fc57fc
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-07-21 16:48:03 +0300

    update inverted index

--- vsm_ir.py
+++ vsm_ir.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
 
+main_dict = {}
 inverted_index = {}
 docs_length = {}
 nltk.download('stopwords')
@@ -20,12 +21,8 @@ ps = PorterStemmer()
 def extract_words_from_file(file_name):
     tree = xml.etree.ElementTree.parse(file_name)
     root = tree.getroot()
-    number_of_docs = 0 ## TODO SHOW TOMER
     for doc in root.findall("RECORD"):
         extract_words_from_doc(doc)
-        number_of_docs += 1
-    inverted_index["D"] = number_of_docs # TODO SHOW TOMER
-
 
 def extract_words_from_doc(doc):
     doc_id, doc_text = "", ""
@@ -38,9 +35,9 @@ def extract_words_from_doc(doc):
 
     text_tokens = tokenizer.tokenize(doc_text)
     docs_length[doc_id] = len(text_tokens)  # saving documents length TODO check the right place
-    filtered_text = filter_stop_words(text_tokens) # filter stop words
-    stemmed_text = words_stemming(filtered_text) # stem text
-    doc_dict = create_doc_dict(stemmed_text) # word to number of appearances in doc
+    filtered_text = filter_stop_words(text_tokens)  # filter stop words
+    stemmed_text = words_stemming(filtered_text)  # stem text
+    doc_dict = create_doc_dict(stemmed_text)  # word to number of appearances in doc
     update_inverted_index(doc_id, doc_dict)
 
 
@@ -49,7 +46,7 @@ def filter_stop_words(text_tokens):
 
 
 def words_stemming(filtered_text):
-    return [ps.stem(w) for w in filtered_text] #TODO check about duplications
+    return [ps.stem(w) for w in filtered_text]  # TODO check about duplications
 
 
 def create_doc_dict(stemmed_text):
@@ -63,15 +60,46 @@ def create_doc_dict(stemmed_text):
 
 
 def update_inverted_index(doc_id, doc_dict):
-    #max_word_num = doc_dict[max(doc_dict, key=doc_dict.get)] TODO for Avi
+    max_word_num = doc_dict[max(doc_dict, key=doc_dict.get)]
     for word in doc_dict:
         if word not in inverted_index:
             inverted_index[word] = {"df": 1,
-                                    "list": {doc_id: doc_dict[word]}}  # the value of doc_id is tf score
+                                    "list": {doc_id: {"f": doc_dict[word], "tf": doc_dict[word] / max_word_num}}}
         else:
             inverted_index[word]["df"] += 1
             linked_list = inverted_index[word]["list"]
-            linked_list[doc_id] = doc_dict[word]
+            linked_list[doc_id] = {"f": doc_dict[word]}
+            linked_list[doc_id]["tf"] = doc_dict[word]/max_word_num
+
+def update_docs_length_dict():
+    number_of_words = 0
+    number_of_docs = len(docs_length)
+    for doc_id in docs_length:
+        number_of_words += docs_length[doc_id]
+
+    docs_length["number_of_docs"] = number_of_docs
+    docs_length["average_doc_length"] = number_of_words/number_of_docs
+
+
+def update_tfidf_score():
+    number_of_docs = docs_length["number_of_docs"]
+    for word in inverted_index:
+        df = inverted_index[word]["df"]
+        linked_list = inverted_index[word]["list"]
+        # compute idf score according to BM25
+        numerator = number_of_docs-df+0.5
+        denominator = df+0.5
+        BM25_idf = math.log(numerator/denominator+1)
+        inverted_index[word]["BM25_idf"] = BM25_idf
+        for doc_id in linked_list:
+            # compute tf*idf score
+            tf = linked_list[doc_id]["tf"]
+            idf = math.log2(number_of_docs/df)
+            linked_list[doc_id]["tf-idf"] = tf*idf
+
+def create_json_file():
+    with open("vsm_inverted_index.json", "w") as inverted_index_json:
+        json.dump(main_dict, inverted_index_json, indent=9)
 
 
 def calculate_query_tf_idf_grade(question, inverted_index):
@@ -94,7 +122,7 @@ def filter_query(question):
 
 def get_tfidf_query_denominator(words_to_tfidf_grade_in_query: dict):
     grades = list(words_to_tfidf_grade_in_query.values())
-    sum_of_sqr = sum([grade**2 for grade in grades])
+    sum_of_sqr = sum([grade ** 2 for grade in grades])
     return math.sqrt(sum_of_sqr)
 
 
@@ -103,18 +131,19 @@ def apply_query_with_tfidf(question, inverted_index):
     question = filter_query(question)
     words_to_tfidf_grade_in_query = calculate_query_tf_idf_grade(question, inverted_index)
     query_denominator = get_tfidf_query_denominator(words_to_tfidf_grade_in_query)
-    docs_lengths = inverted_index["docs_lengths"] # TODO show Tomer
+    docs_lengths = inverted_index["docs_lengths"]  # TODO show Tomer
     for word in question:
         word_relevant_docs_to_grades: dict = inverted_index[word]["list"]
-        for doc in word_relevant_docs_to_grades.keys(): # computing only the numerator
+        for doc in word_relevant_docs_to_grades.keys():  # computing only the numerator
             if doc not in relevent_docs_to_grade:
                 relevent_docs_to_grade[doc] = word_relevant_docs_to_grades[doc] * words_to_tfidf_grade_in_query[word]
             else:
                 relevent_docs_to_grade[doc] += word_relevant_docs_to_grades[doc] * words_to_tfidf_grade_in_query[word]
 
     for doc in relevent_docs_to_grade.keys():
-        relevent_docs_to_grade[doc] = relevent_docs_to_grade[doc] / (docs_lengths[doc] * query_denominator) # TODO show Tomer
-    return sorted(relevent_docs_to_grade.keys(), key=relevent_docs_to_grade.get, reverse=True) # TODO test logic
+        relevent_docs_to_grade[doc] = relevent_docs_to_grade[doc] / (
+                    docs_lengths[doc] * query_denominator)  # TODO show Tomer
+    return sorted(relevent_docs_to_grade.keys(), key=relevent_docs_to_grade.get, reverse=True)  # TODO test logic
 
 
 def apply_query_with_bm(question, inverted_index):
@@ -122,14 +151,16 @@ def apply_query_with_bm(question, inverted_index):
 
 
 def apply_query(ranking_method, path_to_inverted_index, question):
-    inverted_index_as_json = open(path_to_inverted_index,"r")
-    inverted_index = json.load(inverted_index_as_json) # inverted index should be the map as we built it, see if there are changes that needs to be done.
+    inverted_index_as_json = open(path_to_inverted_index, "r")
+    inverted_index = json.load(
+        inverted_index_as_json)  # inverted index should be the map as we built it, see if there are changes that needs to be done.
     if ranking_method == "tfidf":
         sorted_docs = apply_query_with_tfidf(question, inverted_index)
     elif ranking_method == "bm25":
         sorted_docs = apply_query_with_bm(question, inverted_index)
     else:
         print("invalid ranking method argument")
+
 
 if __name__ == "__main__":
     if sys.argv[1] == "create_index":
@@ -138,6 +169,13 @@ if __name__ == "__main__":
             if filename.endswith("xml"):
                 f = os.path.join(path, filename)
                 extract_words_from_file(f)
+
+        update_docs_length_dict()
+        update_tfidf_score()
+        main_dict["inverted_index"] = inverted_index
+        main_dict["docs_length"] = docs_length
+        create_json_file()
+
 
     elif sys.argv[1] == "query":
         ranking_method = sys.argv[2]
